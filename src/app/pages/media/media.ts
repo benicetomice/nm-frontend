3ae0747f1fa9996b86633c0a76d9e51cb10403e8
benicetomice <eressefiriel@gmail.com>
2026-01-21 10:54:49 +0100

    add tabs to media page

--- src/app/pages/media/media.ts
+++ src/app/pages/media/media.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MediaVideos} from '../../components/media-videos/media-videos';
+import {Tab, Tabs, TabList, TabPanel, TabContent} from '@angular/aria/tabs';
+
 
 @Component({
   selector: 'app-media',
   imports: [
-    MediaVideos
+    MediaVideos,
+    TabList,
+    Tab,
+    Tabs,
+    TabPanel,
+    TabContent
   ],
   templateUrl: './media.html',
   styleUrl: './media.css',
 })
 export class Media {
-
 }
