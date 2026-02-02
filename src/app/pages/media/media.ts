9bce36868dcd66030d9205a757be284000bde59e
benicetomice <eressefiriel@gmail.com>
2026-02-02 12:44:08 +0100

    prepare page media for pictures, delete nodejs emailfunction

--- src/app/pages/media/media.ts
+++ src/app/pages/media/media.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {MediaVideos} from '../../components/media-videos/media-videos';
 import {Tab, Tabs, TabList, TabPanel, TabContent} from '@angular/aria/tabs';
+import {MediaPictures} from '../../components/media-pictures/media-pictures';
 
 
 @Component({
@@ -11,7 +12,8 @@ import {Tab, Tabs, TabList, TabPanel, TabContent} from '@angular/aria/tabs';
     Tab,
     Tabs,
     TabPanel,
-    TabContent
+    TabContent,
+    MediaPictures
   ],
   templateUrl: './media.html',
   styleUrl: './media.css',
