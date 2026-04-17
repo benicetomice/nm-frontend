b00b53f32f0e553f9abb9f86ef69bb0252b7bb70
benicetomice <eressefiriel@gmail.com>
2026-04-17 13:09:42 +0200

    nm 32 add gallery control

---
+++ src/app/components/dashboard/gallery-control/gallery-control.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery-control',
+  imports: [],
+  templateUrl: './gallery-control.html',
+  styleUrl: './gallery-control.css',
+})
+export class GalleryControl {
+
+}
